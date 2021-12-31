3b7e1e778e241eec755da7bcdf045f75df07394a
Manohar  Kulat <manoharkulat015@gmail.com>
2021-12-31 12:26:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Thyroid_Detection",
+    version="0.0.3",
+    description="ML project",
+    author="Manohar Kulat",
+    packages=find_packages(),
+    license="MIT"
+)

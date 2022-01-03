619ead5ff36eaf54a58a75520f92933b2af1c774
Manohar  Kulat <manoharkulat015@gmail.com>
2022-01-03 13:25:29 +0530

    changed Procfile

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Thyroid_Detection",
-    version="0.0.3",
+    version="0.0.1",
     description="ML project",
     author="Manohar Kulat",
     packages=find_packages(),

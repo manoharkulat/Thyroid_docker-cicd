e7f9d9ef7ebe5b69deb1dbf70b8151d3f3f30083
Manohar  Kulat <manoharkulat015@gmail.com>
2022-01-06 13:37:45 +0530

    modified setup.py & Dockerfile

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Thyroid_Detection",
-    version="0.0.1",
+    version="0.0.3",
     description="ML project",
     author="Manohar Kulat",
     packages=find_packages(),

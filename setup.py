c20b95bda8a7f3968cbd84c045b24d2669f79045
Manohar  Kulat <manoharkulat015@gmail.com>
2022-01-08 13:35:34 +0530

    set:

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="its a thyroid prediction package",
     author="Manohar",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     license="MIT",
     url="https://github.com/manoharkulat/Thyroid_docker-cicd"
 
